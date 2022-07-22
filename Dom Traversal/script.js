09218455845a5cb14d8681dbb5dcda21cd72b11b
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-07-22 19:55:34 +0700

    add event bubbling in script.js

--- Dom Traversal/script.js
+++ Dom Traversal/script.js
@@ -9,9 +9,9 @@
 
 // Dom Traversal
 
-const close = document.querySelectorAll('.close');
-const nama = document.querySelector('.nama');
-console.log(nama.nextElementSibling);
+// const close = document.querySelectorAll('.close');
+// const nama = document.querySelector('.nama');
+// console.log(nama.nextElementSibling);
 
 // for (let i = 0; i < close.length; i++) {
 // 	close[i].addEventListener('click', function(e) {
@@ -21,13 +21,30 @@ console.log(nama.nextElementSibling);
 // 	})
 // }
 
-// const card = document.querySelectorAll('.card');
-
-close.forEach(function(ele) {
-	ele.addEventListener('click', function(e) {
+// close.forEach(function(ele) {
+// 	ele.addEventListener('click', function(e) {
 // 		console.log(e.target)
-// 		// close[i].parentElement.style.display = 'none';
+// // 		// close[i].parentElement.style.display = 'none';
+// 		e.target.parentElement.style.display = 'none';
+// 		e.preventDefault(); //to stop action in a href 
+// 		e.stopPropagation();
+// 	})
+// })
+
+// const cards = document.querySelectorAll('.card');
+
+// cards.forEach(function(card){
+// 	card.addEventListener('click', function() {
+// 		alert('mantap bosku')
+// 	});
+// });
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function(e) {
+	console.log(e.target);
+	if(e.target.className == 'close') {
 		e.target.parentElement.style.display = 'none';
-		e.preventDefault()
-	})
+		e.preventDefault();
+	}
 })
